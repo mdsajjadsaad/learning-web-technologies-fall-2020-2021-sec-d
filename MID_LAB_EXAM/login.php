80e208acea78425449d3d22ac74e8ea10dc66654
Md Sajjad <mdsajjadsaad@yahoo.com>
2020-11-11 09:17:56 +0600

    login done

---
+++ MID_LAB_EXAM/login.php
@@ -0,0 +1,29 @@
+<?php
+  
+
+?>
+<html>
+<head>
+
+<title>Login</title>
+
+<body>
+    <div class="loginbox">
+
+
+        <form action="" method="post"> 
+           
+            <p>User Id</p>
+            <input id="name" name="username" type="text" name="" placeholder="Enter Username">
+            <p>Password</p>
+            <input id="password" name="password" type="password" name="" placeholder="Enter Password">
+            <input name="submit" type="submit" value="Login">
+            <a href="Register"> Register</a>
+            
+    
+        </form>
+        
+    </div>
+
+</body>
+</head>
